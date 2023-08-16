422f92af997e5d3ef163415c44db110daf6b8935
LatikaMukhi <141509340+LatikaMukhi@users.noreply.github.com>
2023-08-16 08:38:39 +0530

    Create Sum_product_divide_modulus.java

---
+++ Sum_product_divide_modulus.java
@@ -0,0 +1,11 @@
+public class LabManual {
+    public static void main(String[] args) {
+        int a=5;
+        int b=3;
+        System.out.println("The sum of a and b is" + (a+b));
+        System.out.println("The product of a and b is "+ (a*b));
+        System.out.println("After dividing a and b the ans is "+ (a/b));
+        System.out.println("Modulus of a and b is "+ (a%b));
+    }
+    
+}
