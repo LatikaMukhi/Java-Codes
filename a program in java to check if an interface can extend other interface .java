340545e5800662d73b31e2ecaaae976e88e43a85
LatikaMukhi <141509340+LatikaMukhi@users.noreply.github.com>
2023-09-04 11:51:25 +0530

    Create a program in java to check if an interface can extend other interface .java

---
+++ a program in java to check if an interface can extend other interface .java
@@ -0,0 +1,8 @@
+interface Mammal { }
+interface Carnivore extends Mammal { }
+
+public class Main {
+  public static void main(String[] args) {
+System.out.println("Interfaces can extend other interfaces.");
+  }
+}
