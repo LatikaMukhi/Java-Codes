b3146bbcc6dacafe696faf96730470e451321eea
LatikaMukhi <141509340+LatikaMukhi@users.noreply.github.com>
2023-09-04 11:23:45 +0530

    Create Program to print star pattern.java

---
+++ Program to print star pattern.java
@@ -0,0 +1,13 @@
+public class pattern1 {
+    public static void main(String[] args) {
+        char i, j;
+
+        for (i = 1; i <= 5; i++) {
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
