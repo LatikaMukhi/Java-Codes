582806063c129efe8a0e8679497a4ba6c90fb338
LatikaMukhi <141509340+LatikaMukhi@users.noreply.github.com>
2023-09-04 11:55:32 +0530

    Create program outside the above defined package that imports the package PackageDemo and calls its display() function .java

---
+++ program outside the above defined package that imports the package PackageDemo and calls its display() function .java
@@ -0,0 +1,7 @@
+import exercises.java.PackageDemo;
+public class Main {
+    public static void main(String[] args) {
+        PackageDemo packageDemo = new PackageDemo();
+        packageDemo.display();
+    }
+}
